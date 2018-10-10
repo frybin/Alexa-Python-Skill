84a77cceaccabf066efb2335b79beef5d261a3db
frybin <fredrybin@gmail.com>
2018-10-10 16:05:46 -0400

    Initial Commit of Flask App

---
+++ config.env.py
@@ -0,0 +1,11 @@
+import secrets
+from os import environ as env
+
+# Flask config
+IP = env.get('IP', '0.0.0.0')
+PORT = env.get('PORT', 8080)
+SERVER_NAME = env.get('SERVER_NAME', 'rss_skill.csh.rit.edu')
+
+# DB Info
+SQLALCHEMY_DATABASE_URI = env.get('SQLALCHEMY_DATABASE_URI')
+SQLALCHEMY_TRACK_MODIFICATIONS = 'False'
---
+++ rss_skill/__init__.py
@@ -0,0 +1,9 @@
+import os
+from flask import Flask
+app = Flask(__name__)
+
+# Get app config from absolute file path
+if os.path.exists(os.path.join(os.getcwd(), "config.py")):
+    app.config.from_pyfile(os.path.join(os.getcwd(), "config.py"))
+else:
+    app.config.from_pyfile(os.path.join(os.getcwd(), "config.env.py"))
