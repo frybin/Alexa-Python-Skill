14c1885e5cd1b8e029105d91a457f78e2a692edf
frybin <fredrybin@gmail.com>
2018-10-12 20:35:42 -0400

    Quick Fix

--- config.env.py
+++ config.env.py
@@ -3,7 +3,7 @@ from os import environ as env
 # Flask config
 IP = env.get('IP', '0.0.0.0')
 PORT = env.get('PORT', 8080)
-SERVER_NAME = env.get('SERVER_NAME', 'rss_skill.csh.rit.edu')
+SERVER_NAME = env.get('SERVER_NAME', 'rss-skill.csh.rit.edu')
 
 # DB Info
 SQLALCHEMY_DATABASE_URI = env.get('SQLALCHEMY_DATABASE_URI')
